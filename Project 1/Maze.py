4d1d5cde903c206d6dbd9fa8b83eb48bb8d34c8f
Anthony Triolo <anthony@thetriolos.com>
2021-02-06 23:16:20 -0500

    Added strategies 1 and 2

--- Project 1/Maze.py
+++ Project 1/Maze.py
@@ -119,18 +119,23 @@ class EuclideanCoord(Coord):
 
 
 class MazeGame:
-    def __init__(self, dimension: int, prob: float):
+    def __init__(self, dimension: int, prob: float, fire_spread: float, m=np.array([])):
         """initialize the object
         :param dimension: dimension of the matrix that represents the maze
         :param prob: density of blocked cells in the maze
         """
         self._dim = dimension
         self._p = prob  # block density
-        self._m = self.__generate_maze()  # original matrix
+        if len(m) != 0:
+            self._m = m
+        else:
+            self._m = self.generate_maze()  # original matrix
         self._path = []  # list of Coords that form the path
         self._matrix = None  # result matrix
+        self._fire_loc = []
+        self._q = fire_spread
 
-    def __generate_maze(self):
+    def generate_maze(self):
         """generate the matrix for the maze game
         :return: an numpy 2d array with 0s(available cells) and 1s(blocked cells)
         """
@@ -138,6 +143,7 @@ class MazeGame:
         mat[0][0] = 0
         mat[self._dim - 1][self._dim - 1] = 0
         return np.array(mat)
+        
 
     def __is_reachable(self, start: Tuple[int, int], goal: Tuple[int, int], is_dfs: bool):
         """check whether goal(goal) is reachable from s(start) in the maze
@@ -146,6 +152,7 @@ class MazeGame:
         :param is_dfs: True/False for dfs/bfs
         :return whether start and goal are reachable from each other
         """
+        
         self._matrix = self._m.copy()
         m = self._matrix
         if m[start[0]][start[1]] == 1 or m[goal[0]][goal[1]] == 1:
@@ -212,14 +219,84 @@ class MazeGame:
         """getter
         :return: original matrix
         """
-        return self._m
+        return self._m.copy()
 
     def get_result_matrix(self):
         """getter
         :return: None or final matrix
         """
-        return self._matrix
-
+        return self._matrix.copy()
+    
+    def set_fire(self):
+       free_spaces = np.argwhere(self._m == 0)
+       free_spaces = free_spaces[1:-1]
+       index = random.randrange(0, len(free_spaces))
+       free_x = free_spaces[index][0]
+       free_y = free_spaces[index][1]
+       self._m[free_x][free_y] = 3
+       self._fire_loc.append((free_x, free_y))
+       return (free_x, free_y)
+       
+    def start_game(self):
+        self.set_fire()
+        success = self.bfs((0,0), (self._dim-1, self._dim-1))
+        return success
+        
+    def spread_fire(self):
+        free_spaces = np.argwhere(self._m != 1)
+        for space in free_spaces:
+            fire_count = 0
+            x = space[0]
+            y = space[1]
+            free_cell = Coord(self._m, space[0], space[1])
+            coords = [(x, y + 1), (x - 1, y), (x + 1, y), (x, y - 1)]
+            dim = len(self._m)
+            neighbor_coords = [(a, b) for (a, b) in coords if 0 <= a < dim and 0 <= b < dim and self._m[a][b] != 1]
+            neighbors = [Coord(self._m, x, y) for x, y in neighbor_coords]
+            for neighbor in neighbors:
+                if self._m[neighbor.get_coords()[0]][neighbor.get_coords()[1]] == 3:
+                    fire_count += 1
+            if random.uniform(0, 1) <= (1-((1-self._q)**fire_count)):
+                self._fire_loc.append((free_cell.get_coords()[0], free_cell.get_coords()[1]))
+        for fire in self._fire_loc:
+            self._m[fire[0]][fire[1]] = 3
+        
+    def strat_1(self):
+        for point in self._path:
+            self.plot_spread()
+            for fire in self._fire_loc:
+                if point[0] == fire[0] and point[1] == fire[1]:
+                    return False
+            if point[0] == 0 and point[1] == 0:
+                self._m[point[0]][point[1]] = -2
+                continue
+            else:
+                self._m[point[0]][point[1]] = -2
+                self.spread_fire()
+                if point[0] == fire[0] and point[1] == fire[1]:
+                    return False
+                
+        return True
+    
+    def strat_2(self):
+        for point in self._path:
+            if point[0] == 0 and point[1] == 0:
+                self._m[point[0]][point[1]] = -2
+                continue
+            success = self.bfs((point[0],point[1]), (self._dim-1, self._dim-1))
+            self.plot_fire()
+            if success == False:
+                return False
+            for fire in self._fire_loc:
+               if point[0] == fire[0] and point[1] == fire[1]:
+                   return False
+            else:
+                self._m[point[0]][point[1]] = -2
+                self.spread_fire()
+                if point[0] == fire[0] and point[1] == fire[1]:
+                    return False
+        return True                    
+        
     def plot(self):
         """plot the matrix: yellow - visited cells, white - available cells, grey - blocked cells, green - shortest path
         :return: None
@@ -228,12 +305,42 @@ class MazeGame:
         colors = ListedColormap(color_list if len(self._path) > 0 else color_list[:-1])
         plt.matshow(self._matrix, interpolation='none', cmap=colors)
         plt.show()
-
-
+        
+    def plot_fire(self):
+        color_list = ['#800080', '#ffff00', 'w', '#808080', 'g', '#FFA500']  # [-1 yellow, 0 white, 1 grey, 2 green, 3 orange]
+        no_path_colors = ['#ffff00', 'w', '#808080', '#FFA500']
+        colors = ListedColormap(color_list if len(self._path) > 0 else no_path_colors)
+        plt.matshow(self._matrix, interpolation='none', cmap=colors)
+        plt.show()
+        
+    def plot_spread(self):
+        color_list = ['#800080', '#ffff00', 'w', '#808080', 'g', '#FFA500']  # [-1 yellow, 0 white, 1 grey, 2 green, 3 orange]
+        no_path_colors = ['w', '#808080', '#FFA500']
+        colors = ListedColormap(color_list if len(self._path) > 0 else no_path_colors)
+        plt.matshow(self._m, interpolation='none', cmap=colors)
+        plt.show()
+        
 # Test
 if __name__ == '__main__':
     n = 10
-    p = 0.3
-    game = MazeGame(n, p)
-    print(game.a_star((0, 0), (n - 1, n - 1)))
-    game.plot()
+    p = 0.1
+    q = 0.3
+    '''This will be what we use for testing strats
+    for i in range (10): 
+        game = MazeGame(n, p, q)
+        m = game.get_original_matrix()
+        for j in range (10):
+            fire_game = MazeGame(n, p, q, m)
+            fire_game.run_game()
+    '''
+    game = MazeGame(n, p, q)
+    maze = game.get_original_matrix()
+    game.start_game()
+    print("Strat 1")
+    game.strat_1()
+    game_2 = MazeGame(n, p, q, maze)
+    game_2.start_game()
+    print("Strat 2")
+    game_2.strat_2()
+    #print(game.a_star((0, 0), (n - 1, n - 1)))
+    
